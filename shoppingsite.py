bb9988aff57d927080ebe4b1af3b524dfadeec2e
Hackbright Student <info@hackbrightacademy.com>
2015-10-07 17:56:40 -0700

    Working on add_to_cart()

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id(59)
+    melon = melons.get_by_id(melon_id)
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -59,6 +59,7 @@ def show_melon(melon_id):
 @app.route("/cart")
 def shopping_cart():
     """Display content of shopping cart."""
+    session['melon_cart'] = {}
 
     # TODO: Display the contents of the shopping cart.
 
@@ -81,6 +82,7 @@ def add_to_cart(id):
     When a melon is added to the cart, redirect browser to the shopping cart
     page and display a confirmation message: 'Successfully added to cart'.
     """
+
 
     # TODO: Finish shopping cart functionality
 
